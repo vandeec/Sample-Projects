d452fecfee7bbf59632b61a0d874474565607227
Vdeub <loicvdbts1@gmail.com>
2018-06-21 10:52:53 +0100

    Update UnityOgury project

--- Unity/UnityOgury/Assets/testAds.cs
+++ Unity/UnityOgury/Assets/testAds.cs
@@ -1,43 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PresageLib;
 
 
 public class testAds : MonoBehaviour {
 
-	PresageInterstitial placement;
+	PresageInterstitial PresageInterstitial;
 	// Use this for initialization
 	void Awake () {
 		#if UNITY_ANDROID
-		// Creation of our HAndlerImpl to Handle Events
-		InterstitialCallback handlerImpl = new InterstitialCallback();
+		PresageInterstitialImpl presageInterstitialCallback = new PresageInterstitialImpl();
 		// Initializing Presage
-		Presage.Initialize();
-		placement = new PresageInterstitial("82022610-5a50-0135-7016-0242ac120003");
-		placement.SetPresageInterstitialCallback(handlerImpl);
+		Presage.Initialize("270413");
+		PresageInterstitial = new PresageInterstitial("82022610-5a50-0135-7016-0242ac120003");
+		PresageInterstitial.SetPresageInterstitialCallback(presageInterstitialCallback);
 		#endif
 	}
 
 	public void Load(){
 		#if UNITY_ANDROID
 		// Making load call to try to load an Ad 
-		placement.Load();
+		PresageInterstitial.Load();
 		#endif
 	}
 
-   public void	AdToServe(){
-		
-		#if UNITY_ANDROID
-		// Making AdToServe call to try to show an Ad if available
-		placement.AdToServe();
-		#endif
-	}
-
-	public void	canShow(){
+	public void	isLoaded(){
 
 		#if UNITY_ANDROID
 		// Making CanShow call to check ad availability
-		placement.CanShow();
+		PresageInterstitial.IsLoaded();
 		#endif
 	}
 
@@ -46,39 +38,36 @@ public class testAds : MonoBehaviour {
 		#if UNITY_ANDROID
 
 		// Making Show call to show an ad.
-		if (placement.CanShow())
-			placement.Show();
+		if (PresageInterstitial.IsLoaded())
+			PresageInterstitial.Show();
 		#endif
 	}
 
 }
 
 
-public class InterstitialCallback : PresageInterstitial.PresageInterstitialCallback  {
+public class PresageInterstitialImpl : PresageInterstitial.PresageInterstitialCallback {
 
-	public void OnAdAvailable() {
-		Debug.Log("ad available");
-	}
-
-	public void OnAdNotAvailable() {
-		Debug.Log("no ad available");
-	}
-
-	public void OnAdLoaded() {
-		Debug.Log("an ad in loaded, ready to be shown");
-	}
-
-	public void OnAdDisplayed() {
-		Debug.Log("ad displayed");
-	}
-
-	public void OnAdClosed() {
-		Debug.Log("ad closed");
-	}
-
-	public void OnAdError(int code) {
-		Debug.Log("error with code" + code);
-	}
-
+    public void OnAdAvailable() {
+        Debug.Log("interstitial available");
+    }
+    public void OnAdNotAvailable() {
+        Debug.Log("no interstitial available");
+    }
+    public void OnAdLoaded() {
+        Debug.Log("an interstitial is loaded, ready to be shown");
+    }
+    public void OnAdDisplayed() {
+        Debug.Log("interstitial displayed");
+    }
+    public void OnAdClosed() {
+        Debug.Log("interstitial closed");
+    }
+    public void OnAdError(int code) {
+        Debug.Log("interstitial on error with code: " + code);
+    }
+    public void OnAdNotLoaded() {
+        Debug.Log("no interstitial loaded");
+    }
 }
 
--- Unity/UnityOgury/README.md
+++ Unity/UnityOgury/README.md
@@ -5,7 +5,8 @@ Before using any projects, go to your account on the [Admin Ogury Website](https
 ## Instructions
 
 #### [Ogury Basic Integration](https://github.com/Ogury/Sample-Projects/tree/master/Unity/UnityOgury)
-* Change your Api_key in the `AndroidManifest.xml` [here](https://github.com/Ogury/Sample-Projects/blob/master/Unity/UnityOgury/Assets/Plugins/Android/AndroidManifest.xml#L36)
+* Change your app_api_key in the `testAds.cs` [here](https://github.com/Ogury/Sample-Projects/blob/master/Unity/UnityOgury/Assets/testAds.cs#L15)
+* Change your interstitial ad_unit in the `testAds.cs` [here](https://github.com/Ogury/Sample-Projects/blob/master/Unity/UnityOgury/Assets/testAds.cs#L16)
 
 ## Documentation
 
